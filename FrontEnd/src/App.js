930c6d836d40bbcfead1d36b7dab00e5ff19be2e
Muhammad Usman <128576571+i-usman-56@users.noreply.github.com>
2024-03-18 23:27:36 +0500

    changes

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -5,15 +5,24 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-
+import { useState, useEffect } from 'react';
+const txtFile = '../public/app-adds.txt'
 const queryClient = new QueryClient()
 
 function App() {
+  // Function to fetch text file content
+  const fetchTextFile = () => {
+    return fetch(txtFile)
+      .then((response) => response.text())
+      .catch((error) => console.error('Error fetching text file:', error));
+  };
+
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
         <Routes>
-          <Route path='/' Component={Home} />
+          <Route path='/' element={<Home />} /> {/* Make sure to use `element` instead of `Component` */}
+          <Route path='/app-ads.txt' element={<TextFileDisplay fetchTextFile={fetchTextFile} />} />
         </Routes>
       </Router>
       <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
@@ -21,5 +30,16 @@ function App() {
     </QueryClientProvider>
   );
 }
+
+// Component to display the text file
+const TextFileDisplay = ({ fetchTextFile }) => {
+  const [textFileContent, setTextFileContent] = useState('');
+
+  useEffect(() => {
+    fetchTextFile().then(setTextFileContent);
+  }, [fetchTextFile]);
+
+  return <pre>{textFileContent}</pre>; // Using <pre> to preserve formatting
+};
 
 export default App;
